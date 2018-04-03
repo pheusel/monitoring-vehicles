5f8265c2cd24f9742d71fa8c33e36631bd38242d
pheusel <philipp.heusel@posteo.de>
2018-04-03 08:47:35 +0200

    current project status

--- vehicles/src/main/java/dhbw/wpvs/vehicle/WGS84.java
+++ vehicles/src/main/java/dhbw/wpvs/vehicle/WGS84.java
@@ -1,13 +1,4 @@
-/*
- * Copyright © 2018 Dennis Schulmeister-Zimolong
- * 
- * E-Mail: dhbw@windows3.de
- * Webseite: https://www.wpvs.de/
- * 
- * Dieser Quellcode ist lizenziert unter einer
- * Creative Commons Namensnennung 4.0 International Lizenz.
- */
-package dhbwka.wwi.vertsys.pubsub.fahrzeug;
+package dhbw.wpvs.vehicle;
 
 /**
  * Eine Koordinate nach WGS84-Standard. Vgl.
